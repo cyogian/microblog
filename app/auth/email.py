66475f3be0ae088a0c84d50dd9385b56941aa3d7
Amar Nath Yogi <immortal2907@gmail.com>
2019-12-04 13:56:29 +0530

    uploading

--- app/auth/email.py
+++ app/auth/email.py
@@ -1,11 +1,12 @@
 from flask import render_template
 from flask_babel import lazy_gettext as _l
+from flask_babel import _
 from flask import current_app
 from app.email import send_mail
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_mail(
-        subject=_l("[Microblog] Reset Your Password"), 
+        subject=_("[Microblog] Reset Your Password"), 
         sender=current_app.config['ADMINS'][0], 
         recipients=[user.email],
         text_body=render_template("email/reset_password.txt", user=user, token=token), 
@@ -15,7 +16,7 @@ def send_password_reset_email(user):
 def send_email_verification_email(user):
     token = user.get_email_verification_token()
     send_mail(
-        subject=_l("[Microblog] Verify Your Email"), 
+        subject=_("[Microblog] Verify Your Email"), 
         sender=current_app.config['ADMINS'][0], 
         recipients=[user.email],
         text_body=render_template("email/verify_email.txt", user=user, token=token), 
