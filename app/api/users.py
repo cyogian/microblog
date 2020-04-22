077c72e95ff244f53716c8dd7bca632bd253ad87
Amar Nath Yogi <immortal2907@gmail.com>
2020-04-22 21:49:15 +0530

    getUsers function to not include currentUser

--- app/api/users.py
+++ app/api/users.py
@@ -32,7 +32,8 @@ def get_user(id):
 def get_users():
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', 10, type=int), 100)
-    data = User.to_collection_dict(User.query, page, per_page, 'api.get_users')
+    data = User.to_collection_dict(User.query.filter(
+        User.id != g.current_user.id), page, per_page, 'api.get_users')
     return jsonify(data)
 
 
